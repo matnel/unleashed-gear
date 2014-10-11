ca80b34a575b7e0a27fb7ed67757801f2e4ff4a9
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-10-11 16:00:55 +0300

    Functionalize the code a bit

--- run.js
+++ run.js
@@ -1,13 +1,15 @@
 $().ready( function() {
 	
-	var app = tizen.application.getCurrentApplication();
-	app = app.appInfo.id; 
+	function runEvery( minutes ) {
+		var app = tizen.application.getCurrentApplication();
+		app = app.appInfo.id; 
+		
+		var time = 10 * tizen.alarm.PERIOD_MINUTE;
+		time = new tizen.AlarmRelative( time );
+		
+		tizen.alarm.add( time, app );
+	}
 	
-	var time = 10 * tizen.alarm.PERIOD_MINUTE;
-	time = new tizen.AlarmRelative( time );
-	
-	console.log( app );
-	
-	tizen.alarm.add( time, app );
+	runEvery( 10 );
 	
 } );
