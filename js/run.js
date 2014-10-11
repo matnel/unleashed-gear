80604351f80237f67c2fb30d769e926a9c8da6f3
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-10-11 20:03:55 +0300

    Typofix

--- js/run.js
+++ js/run.js
@@ -4,7 +4,7 @@ $().ready( function() {
 		var app = tizen.application.getCurrentApplication();
 		app = app.appInfo.id; 
 		
-		var time = 10 * tizen.alarm.PERIOD_MINUTE;
+		var time = minutes * tizen.alarm.PERIOD_MINUTE;
 		time = new tizen.AlarmRelative( time );
 		
 		tizen.alarm.add( time, app );
