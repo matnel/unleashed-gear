51b9499a0f64b39a392ab71fc21bd25be129dd4b
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-10-11 10:13:37 +0300

    Initial commit

---
+++ sensors.js
@@ -0,0 +1,31 @@
+/** react to shaking **/
+
+window.ondevicemotion = function( data ) {
+	
+	var sum = 0;
+	sum += Math.abs( data.acceleration.x );
+	sum += Math.abs( data.acceleration.y );
+	sum += Math.abs( data.acceleration.z );
+	
+	if( sum > 15 ) {
+		var e = document.createEvent("HTMLEvents");
+		e.initEvent("shake", true, true);
+		document.dispatchEvent( e );
+	}
+}
+
+pedometer = (tizen && tizen.humanactivitymonitor) ||
+(window.webapis && window.webapis.motion) || null;
+
+console.log("ready to pedo");
+
+pedometer.start( 'PEDOMETER',
+		function onSuccess(data) {
+			var e = document.createEvent("HTMLEvents");
+			e.initEvent("pedometer", true, true);
+			e.data = data;
+			document.dispatchEvent( e );
+		}
+);
+
+ console.log("pedo started");
