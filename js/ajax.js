2cadb6b454dfc2164d231f56a32b73ab6dd49a82
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-11-01 15:03:19 +0200

    Remove debug msg

---
+++ js/ajax.js
@@ -0,0 +1,19 @@
+/***
+ Create a fake AJAX connection using the device as a proxy.
+ ***/
+$().ready( function() {
+	var agent = null;
+	/*
+	webapis.sa.requestSAAgent( function(agents) {
+		
+		console.log("searching for agents");
+		
+		agent = agents[0];
+		
+		for (var i = 0; i < agents.length; i++)  {
+			console.log(i + ". " + agents[i].name);
+		}
+	});
+	*/
+	
+});
