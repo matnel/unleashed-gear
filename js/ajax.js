21dd87303e553956fac5e6f72eb98a3bd7afb2e2
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-11-15 22:09:24 +0200

    Make fake ajax work

--- js/ajax.js
+++ js/ajax.js
@@ -2,18 +2,50 @@
  Create a fake AJAX connection using the device as a proxy.
  ***/
 $().ready( function() {
+
 	var agent = null;
-	/*
-	webapis.sa.requestSAAgent( function(agents) {
-		
-		console.log("searching for agents");
-		
-		agent = agents[0];
-		
-		for (var i = 0; i < agents.length; i++)  {
-			console.log(i + ". " + agents[i].name);
+	var socket = null;
+	var CHANNELID = 104;
+	var ProviderAppName = "HelloAccessoryProvider";
+
+	var agentCallback = {
+			onconnect : function(socket) {
+				socket = socket;
+
+				socket.setSocketStatusListener(function(reason){
+					console.log("Service connection lost, Reason : [" + reason + "]");
+					disconnect();
+				});
+
+			},
+			onerror : $.noop
+		};
+
+	var peerAgentFindCallback = {
+			onpeeragentfound : function(peerAgent) {
+				try {
+					if (peerAgent.appName == ProviderAppName) {
+						agent.setServiceConnectionListener( agentCallback );
+						agent.requestServiceConnection(peerAgent);
+					}
+				} catch(err) {
+					console.log("exception [" + err.name + "] msg[" + err.message + "]");
+				}
+			},
+			onerror : $.noop
 		}
+
+	webapis.sa.requestSAAgent( function(agents) {
+		agent = agents[0];
+		agent.setPeerAgentFindListener( peerAgentFindCallback );
+		agent.findPeerAgents();
+
 	});
-	*/
-	
+
+	window.sendAjax = function( url , data, callback ) {
+		SASocket.setDataReceiveListener( callback );
+		SASocket.sendData(CHANNELID,  url );
+	}
+
+
 });
